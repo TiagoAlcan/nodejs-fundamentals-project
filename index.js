a1af7a1aa00e9baaee54195d6624119cfdddd025
TiagoAlcan <tiago.gomesalcan@gmail.com>
2025-08-05 11:29:15 -0300

    feat: add express and routes - other form to start a server

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const person = new Person("John Doe");
 
 // require("./modules/fs");
 
-require("./modules/http");
+// require("./modules/http");
+
+require("./modules/express");
 
 console.log(person.sayMyName());
---
+++ modules/express.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/home", (req, res) => {
+  res.contentType("application/html"); // Definindo o tipo de conteúdo como HTML (no Json a gente não define o tipo de conteúdo)
+  res.status(200).send("<h1>Hello, World!<h1>"); // Resposta simples para a rota raiz - tem que ser uma string
+});
+
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+    },
+    {
+      name: "Jane Doe",
+      email: "janedoe@gmail.com",
+    },
+  ];
+  res.status(200).json(users); // Enviando a resposta como JSON
+  // Não é necessário definir o tipo de conteúdo, pois o Express já faz isso automaticamente
+});
+
+app.listen(port, () => {
+  console.log(`Server running with Express at http://localhost:${port}/`);
+});
